83654cdd7999789c0d18160e7ce5630ac2ffdefa
lilcheatcod <joshuauzohuo@gmail.com>
2025-08-09 23:52:07 +0200

    Rename procfile to Procfile and update app.py

--- app.py
+++ app.py
@@ -112,5 +112,6 @@ def generate_audio():
         return jsonify({"error": str(e)}), 500
     
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
-   
+    PORT = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=PORT)
+       
